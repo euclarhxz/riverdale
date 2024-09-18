f2060bb0f52ed9f3427ad759e9bb8df2110b5fb9
euclarhxz <160048272+euclarhxz@users.noreply.github.com>
2024-09-18 14:59:27 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,26 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+criaCartao(
+    'Historia',
+    'Onde surgiram os primeiros ancestrais do homem?',
+    'Pelo que sabemos, os humanos surgiram na África Oriental há cerca de 2,5 milhões de anos, no final do Plioceno, período que compreende de 5 a 2 milhões atrás. '
+)
+
+criaCartao(
+    'Portugues',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+criaCartao(
+    'Portugues',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'Portugues',
+    'Onde se localiza Machu Picchu? ',
+    ''
+)
+
